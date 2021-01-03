3a8f06e6fc16b5c37a454d7190e217f7573c64e4
Harry Kantas <harrykantas@gmail.com>
2021-01-03 10:34:01 +0000

    added new functions and abbr

---
+++ .config/fish/functions/ansible-run.fish
@@ -0,0 +1,10 @@
+function ansible-run
+  cd $HOME/Projects/ansible-playbooks
+  switch $argv[1]
+    case update_all
+      command ansible-playbook -kK -v playbooks/archlinux_update.yml --tags yay $argv[2]
+    case backup_ssh
+      command ansible-playbook -kK -v -l (hostname) playbooks/backup.yml --tags backup $argv[2]
+  end
+  cd -
+end
