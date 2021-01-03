f24d028108f2ca7544ec8b1cbc2ddca8bad5ec23
Harry Kantas <harrykantas@gmail.com>
2021-01-03 19:41:05 +0000

    changed tag for ansible-run

--- .config/fish/functions/ansible-run.fish
+++ .config/fish/functions/ansible-run.fish
@@ -2,7 +2,7 @@ function ansible-run
   cd $HOME/Projects/ansible-playbooks
   switch $argv[1]
     case update_all
-      command ansible-playbook -kK -v playbooks/archlinux_update.yml --tags yay $argv[2]
+      command ansible-playbook -kK -v playbooks/archlinux_update.yml --tags update_packages $argv[2]
     case backup_ssh
       command ansible-playbook -kK -v -l (hostname) playbooks/backup.yml --tags backup $argv[2]
   end
