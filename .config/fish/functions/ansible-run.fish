a818e8f1c3d9d24efec55202c5ceeeb91948f6e8
Harry Kantas <harrykantas@gmail.com>
2021-01-08 11:50:09 +0000

    changes in fish functions

--- .config/fish/functions/ansible-run.fish
+++ .config/fish/functions/ansible-run.fish
@@ -2,9 +2,9 @@ function ansible-run
   cd $HOME/Projects/ansible-playbooks
   switch $argv[1]
     case update_all
-      command ansible-playbook -kK -v playbooks/archlinux_update.yml --tags update_packages $argv[2]
+      ANSIBLE_STDOUT_CALLBACK=debug ansible-playbook -kK -v playbooks/archlinux_update.yml --tags update_packages $argv[2..-1]
     case backup_ssh
-      command ansible-playbook -kK -v -l (hostname) playbooks/backup.yml --tags backup $argv[2]
+      ANSIBLE_STDOUT_CALLBACK=debug ansible-playbook -kK -v -l (hostname) playbooks/backup.yml --tags backup $argv[2..-1]
   end
   cd -
 end
