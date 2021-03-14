de8d7500033f3866d7b6803a26146f8ba53ac7f7
Harry Kantas <harrykantas@gmail.com>
2021-03-14 11:43:05 +0000

    wireguard function

--- .config/fish/functions/home-net.fish
+++ .config/fish/functions/home-net.fish
@@ -1,10 +1,14 @@
 function home-net
   switch $argv[1]
     case up connect start
-      wg-quick up wg0
+      sudo wg-quick up wg0
     case down disconnect stop
-      wg-quick down wg0
+      sudo wg-quick down wg0
     case status show
-      sudo wg show
+      if test (ip link | grep -c wg0) -eq 0
+        echo "Interface wg0 is not up."
+      else
+        sudo wg show
+      end
   end
 end
