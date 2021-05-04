a485e7831c7fd3395fc56d3208c937c7bfab10fd
Harry Kantas <harrykantas@gmail.com>
2021-05-04 23:07:39 +0100

    added hide-application function with completions

--- .config/fish/functions/home-net.fish
+++ .config/fish/functions/home-net.fish
@@ -4,6 +4,9 @@ function home-net
       sudo wg-quick up wg0
     case down disconnect stop
       sudo wg-quick down wg0
+    case restart reset
+      sudo wg-quick down wg0
+      sudo wg-quick up wg0
     case status show
       if test (ip link | grep -c wg0) -eq 0
         echo "Interface wg0 is not up."
