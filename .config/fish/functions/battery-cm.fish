0debf7a42523256dd084f2467c2b33a8e263aa8b
Harry Kantas <harrykantas@gmail.com>
2022-05-07 20:59:30 +0100

    removed unused functions

--- .config/fish/functions/battery-cm.fish
+++
@@ -1,8 +0,0 @@
-function battery-cm
-  switch $argv[1]
-    case enable disable
-      command ideapad-cm $argv[1] acpi_call
-    case '*'
-      echo "Usage: battery-cm enable|disable"
-  end
-end
--- .config/fish/functions/brightness.fish
+++
@@ -1,5 +0,0 @@
-function brightness
-  if set -q argv[1]
-    command gdbus call --session --dest org.gnome.SettingsDaemon.Power --object-path /org/gnome/SettingsDaemon/Power --method org.freedesktop.DBus.Properties.Set org.gnome.SettingsDaemon.Power.Screen Brightness "<int32 $argv[1]>"
-  end
-end
